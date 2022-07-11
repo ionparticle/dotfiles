9100cc42c3b14c646c1bdf9836c9ed708d13239d
John Hsu <ionparticle@gmail.com>
2022-07-10 20:27:19 -0700

    EDIT freshinstall script, more stuff to install
    Also backed up some conf files that I sometimes forget to backup.

--- freshinstall.sh
+++ freshinstall.sh
@@ -3,18 +3,34 @@
 # setting up a new home system from scratch
 
 ESSENTIAL_PACKAGES=('vim-gtk' 'git' 'stow' 'curl')
-STORAGE_PACKAGES=('mergerfs' 'snapraid')
+STORAGE_PACKAGES=('mergerfs' 'snapraid' 'gparted')
+# dkms is needed to install vendor-reset modules
 VFIO_PACKAGES=('qemu-kvm' 'qemu-utils' 'libvirt-daemon-system' \
-    'libvirt-clients' 'bridge-utils' 'virt-manager' 'ovmf')
+    'libvirt-clients' 'bridge-utils' 'virt-manager' 'ovmf' 'dkms' 'samba')
 # thunar-archive-plugin & xarchiver - operations via right click context menu
-# zip handling should already be installed
+# xarchiver unfortunately prefer p7zip over unar for dealing with rar files, so
+# we have to decompress rar files with command line unar
 # p7zip-full - for 7z handling
-# unar - handles rar & is free (not used by mcomix, so mcomix can't handle rar)
-ARCHIVER_PACKAGES=('xarchiver' 'thunar-archive-plugin' 'unar' 'p7zip-full')
+# unar - unfortunately, while unar has a free implementation of rar extract,
+#        it is unable to handle newer rar compressions
+# p7zip-rar - non-free, but can handle newer rar compressions
+ARCHIVER_PACKAGES=('xarchiver' 'thunar-archive-plugin' 'p7zip-full' 'p7zip-rar')
 # viewnior - image viewer
-MEDIA_PACKAGES=('mcomix' 'mpv' 'viewnior')
+# evince - pdf viewer
+MEDIA_PACKAGES=('gimp' 'mcomix' 'mpv' 'viewnior' 'evince' 'mkvtoolnix')
+CAD_PACKAGES=('freecad' 'openscad')
 GAMING_PACKAGES=('steam')
 INTERNET_PACKAGES=('filezilla')
+# hddtemp gets pulled in but needs workaround: sudo chmod +s /usr/sbin/hddtemp
+SENSOR_PACKAGES=('xfce4-sensors-plugin' 'xfce4-systemload-plugin' 'xfce4-netload-plugin' 'xsensors' 'smartmontools')
+TOOL_PACKAGES=('byobu' 'aptitude')
+
+# Manual
+# NoiseTorch - real-time microphone noise suppression
+# Firefox - get rid of snap
+# Samba - copy smb.conf
+# setup samba share user password
+# sudo smbpasswd -a john
 
 installPackages() {
     packages=("$@")
@@ -34,12 +50,20 @@ echo "---- ARCHIVER PACKAGES ----"
 installPackages "${ARCHIVER_PACKAGES[@]}"
 echo "---- MEDIA PACKAGES ----"
 installPackages "${MEDIA_PACKAGES[@]}"
+echo "---- CAD PACKAGES ----"
+installPackages "${CAD_PACKAGES[@]}"
 echo "---- GAMING PACKAGES ----"
 installPackages "${GAMING_PACKAGES[@]}"
 echo "---- INTERNET PACKAGES ----"
 installPackages "${INTERNET_PACKAGES[@]}"
+echo "---- SENSOR PACKAGES ----"
+installPackages "${SENSOR_PACKAGES[@]}"
+echo "---- TOOL PACKAGES ----"
+installPackages "${TOOL_PACKAGES[@]}"
 
 echo "---- EXTERNAL (NON-APT) PROGRAMS ----"
+# need to ensure local bin exists first
+mkdir -p ~/.local/bin/
 YTDLP_FILE=~/.local/bin/yt-dlp
 if [ ! -f "$YTDLP_FILE" ]; then
     echo "yt-dlp needs to be downloaded"
@@ -49,10 +73,12 @@ else
     echo "yt-dlp already installed"
 fi
 
+# TODO: add install btop, prusaslicer
+
 echo "---- SETUP STORAGE DIRS ----"
 (cd /mnt && \
  sudo mkdir -p parity1 parity2 parity3 data data1 data2 data3 data4 data5 \
-               data6 && \
+               data6 data7 && \
  sudo chown john * && \
  echo "storage dirs setup")
 
@@ -61,3 +87,8 @@ echo "---- CONF ----"
 # terrible, and the setting to disable it isn't even configurable in UI
 echo "setting show just directory name in Thunar window title"
 xfconf-query --channel thunar --property /misc-full-path-in-title  --create --type bool --set false
+
+# when flashing arduino firmware, we need to be in the dialout group or we'll
+# get access denied when trying to access the usb to serial port /dev/ttyACM0
+echo "add user to dialout group"
+sudo adduser john dialout
